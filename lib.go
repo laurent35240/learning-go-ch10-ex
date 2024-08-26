85329a38b87836f4451321a7deaaacef5af9113a
Laurent Clouet <laurent.clouet@westwing.pl>
2024-08-26 14:10:58 +0200

    Add documentation

--- lib.go
+++ lib.go
@@ -1,5 +1,15 @@
+// Package ex can perform some simple math operations
+// Part of learning go ch10 exercises
 package ex
 
+// Add returns the sum of two integers
+// This is a simple function that adds two integers
+// and returns the result
+// Example:
+//
+//	Add(1, 2) // 3
+//
+// [Addition doc]: https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
